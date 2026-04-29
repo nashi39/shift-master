841ad4a72b08c7dff4405e9883859640ad0338cd
nashi39 <remmen3@outlook.jp>
2026-04-29 18:03:58 +0900

    main,indexにコメントを追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css' // グローバルスタイルのインポート
+import App from './App.jsx' // メインのアプリケーションコンポーネント
 
+/**
+ * アプリケーションのエントリーポイント
+ * 
+ * HTMLファイル内の <div id="root"></div> に対して、
+ * Reactアプリケーションをレンダリング（描画）します。
+ * 
+ * StrictMode: 開発中に潜在的な問題を検知するためのラッパーです。
+ */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
